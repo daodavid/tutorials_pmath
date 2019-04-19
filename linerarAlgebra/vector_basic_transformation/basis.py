5ef33c7b8c673285c76b5dd6a0dbd578a8aa8497
david <david.stankov@haemimont.com>
2019-04-19 16:13:55 +0300

    vector transformation throw bases init

--- linerarAlgebra/vector_basic_transformation/basis.py
+++ linerarAlgebra/vector_basic_transformation/basis.py
@@ -22,7 +22,7 @@ def get_cordinate(v, basic, tp="array"):
         return x_component[0], y_component[0]
 
 
-def append_vector_plot(x, y, plt):
+def append_vector_plot(x, y, plt,color):
     z = get_projection_array(x, y)
     line1, = ax.plot(z[0], z[1], label='Using set_dashes()', color='green')
     line1.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
@@ -31,7 +31,7 @@ def append_vector_plot(x, y, plt):
     line2, = ax.plot(z[1], z[0], label='Using set_dashes()', color='green')
     line2.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
 
-    ax.arrow(0, 0, x, y, head_width=0.3, head_length=0.2, fc='black', ec='black')
+    ax.arrow(0, 0, x, y, head_width=0.3, head_length=0.2, fc=color, ec=color)
 
 
 basis = np.array([[1, 0],
@@ -45,10 +45,28 @@ x, y = get_cordinate(v1, basis, "cm")
 ax = plt.axes()
 ax.arrow(0, 0, 10, 0, head_width=0.3, head_length=0.2, fc='black', ec='black')  # create e1 axes
 ax.arrow(0, 0, 0, 10, head_width=0.3, head_length=0.2, fc='black', ec='black')  # create e2 axes
-plt.xlim(0, 10)  # show measure
-plt.ylim(0, 10)
-append_vector_plot(x,y,plt)
+plt.xlim(-10, 10)  # show measure
+plt.ylim(-10, 10)
 
+
+
+append_vector_plot(x,y,plt,'blue')
+
+'''
+let to create other base
+'''
+
+e1 = [1,1]
+e2 = [-1,1]
+T = np.array([e1,e2])
+#to find the cordiante of x to basic e1 e2
+
+x,y = get_cordinate(v1, T, "cm")
+
+z =  T.dot([[x],
+            [y]])
+
+append_vector_plot(z[0,0],z[1,0],plt,'red')
 plt.show()
 
 """
