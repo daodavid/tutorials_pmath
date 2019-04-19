6176db993d595355982db7567e360798588fc165
david <david.stankov@haemimont.com>
2019-04-19 16:55:03 +0300

    added function draw line between to vectors

--- linerarAlgebra/vector_basic_transformation/basis.py
+++ linerarAlgebra/vector_basic_transformation/basis.py
@@ -2,6 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def append_line(fitst_points, second_points,plt):
+    x1 = fitst_points[0]
+    y1 = fitst_points[1]
+
+    x2 = second_points[0]
+    y2 = second_points[1]
+    tg = (y2 - y1) / (x2 - x1)
+    x = np.linspace(x1, x2, 10)
+    f = lambda z: y1 + tg * z
+    y = [f(i) for i in x]
+    line2, = ax.plot(x, y, label='Using set_dashes()', color='red')
+    line2.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
+
+
+
 def get_projection_array(x, y):
     x = np.linspace(0, x, 10)
     y = [y for i in range(10)]
@@ -22,7 +37,7 @@ def get_cordinate(v, basic, tp="array"):
         return x_component[0], y_component[0]
 
 
-def append_vector_plot(x, y, plt,color):
+def append_vector_plot(x, y, plt, color):
     z = get_projection_array(x, y)
     line1, = ax.plot(z[0], z[1], label='Using set_dashes()', color='green')
     line1.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
@@ -48,28 +63,32 @@ ax.arrow(0, 0, 0, 10, head_width=0.3, head_length=0.2, fc='black', ec='black')  
 plt.xlim(-10, 10)  # show measure
 plt.ylim(-10, 10)
 
-
-
-append_vector_plot(x,y,plt,'blue')
+append_vector_plot(x, y, plt, 'blue')
 
 '''
 let to create other base
 '''
 
-e1 = [1,1]
-e2 = [-1,1]
-T = np.array([e1,e2])
-#to find the cordiante of x to basic e1 e2
+e1 = [1, 1]
+e2 = [-1, 1]
+T = np.array([e1, e2])
+# to find the cordinate of x to basic e1 e2
 
-x,y = get_cordinate(v1, T, "cm")
+x, y = get_cordinate(v1, T, "cm")
 
-z =  T.dot([[x],
-            [y]])
+z = T.dot([[x],
+           [y]])
 
-append_vector_plot(z[0,0],z[1,0],plt,'red')
+append_vector_plot(z[0, 0], z[1, 0], plt, 'red')
+append_line([2,4],[5,7],plt)
 plt.show()
 
 """
 let to create other basic
+
+"""
+"""
+let's to make something deep 
+to proof codinates on basic to another
 
 """
