0934ea0cab5ec23811b15df8a24dc2c9a9ddbf19
david <david.stankov@haemimont.com>
2019-04-22 17:13:35 +0300

    some exercise

--- linerarAlgebra/vector_basic_transformation/transformation.py
+++ linerarAlgebra/vector_basic_transformation/transformation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.linalg import inv
 
 
 class Vector_Base():
@@ -22,23 +23,43 @@ class Vector_Base():
     def get_cordinate(self, vector):
         v = np.array([vector[0], vector[1]])
         # v = np.array([vector[0,0],vector[1,0]])
-        result = self.array.dot(v)
-        return result
+        invr = self.get_inverse()
+        return  invr.dot(v)
+
 
     @classmethod
-    def plot_vector(cls,vector,color='black'):
-        print(vector)
+    def plot_vector(cls, vector, color='black',start=0,lenght=0.1):
+        ax = plt.axes()
+        ax.arrow(start, start, vector[0], vector[1], head_width=0.1, head_length=lenght, color=color)
+
+    def get_inverse(self):
+        return inv(self.array)
 
 
 vector_basic=Vector_Base([[1, 0],
                            [0, 1]])
 vector_basic.plotBasic()
 result = vector_basic.get_cordinate([1, 4])
-Vector_Base.plot_vector(result)
+Vector_Base.plot_vector(result,'green',0,6)
 vector_basic1=Vector_Base([[1,1],
                          [-1, 1]])
 vector_basic1.plotBasic()
-result = vector_basic.get_cordinate([1, 4])
-Vector_Base.plot_vector(result)
+result2 = vector_basic1.get_cordinate([1, 4])
+result = np.array([result[0],
+                  result[1]])
+result2 = vector_basic1.array.dot(result2)
+Vector_Base.plot_vector(result2,'blue',0,0.9)
 print(result)
 plt.show()
+
+# vector_basic1 = Vector_Base([[1, 1],
+#                              [-1, 1]])
+#
+# result = vector_basic1.get_cordinate([1, 4])
+#
+# Vector_Base.plot_vector(result)
+# Vector_Base.plot_vector([1,4])
+# plt.show()
+#
+#
+# print(result)
