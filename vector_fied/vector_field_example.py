86ceff72020a7135f85fec282970bf5249fdfbe9
david <david.stankov@haemimont.com>
2019-04-23 16:59:02 +0300

    Vecotor Field

---
+++ vector_fied/vector_field_example.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def append_vector(x_start, y_start, x, y,color='blue'):
+    plt.quiver(x_start, y_start, x, y, angles='xy', scale_units='xy', scale=5, color='blue')
+    # plt.quiver([0], [0], [2 , angles='xy', scale_units='xy', scale=1)
+
+
+def draw_vectot_field(x_funct, y_funct, range_X, range_Y, color='blue'):
+    """
+    r = x*x(e1) + y*x(e2)
+
+    :param x_funt:
+    :param y_funct:
+    :param range_X:
+    :param range_Y:
+    :param color:
+    :return:
+    """
+    plt.xlim(range_X[0], range_X[1])
+    plt.ylim(range_Y[0], range_Y[0])
+    plt.axis('equal')
+    x_arg = np.linspace(range_X[0], range_X[1], 10)
+    y_args = np.linspace(range_Y[0], range_Y[1], 10)
+
+    for i in x_arg:
+        for j in y_args:
+            x = x_funct(i, j)
+            y = y_funct(i, j)
+            append_vector(i, j, x, y)
+
+    plt.show()
+# add = lambda x, y : x + y
+
+
+f_x = lambda x,y : x
+f_y = lambda x,y : y
+draw_vectot_field(f_x,f_y,[-100,100],[-100,100])
